7dfdbb8764f4447271aa4100661df3d4024976c5
orange4glace <orange4glace@gmail.com>
2018-12-21 00:09:40 +0900

    pointer test code updated

--- test/c/pointer.c
+++ test/c/pointer.c
@@ -1,8 +1,10 @@
 void main() {
-  int arr[10];
-  int *ptr = (int*)arr;
+  int *arr[10];
+  int **ptr = (int**)arr;
+  for (int i = 0; i < 10; i ++) {
+    ptr[i] = new int();
+    *ptr[i] = i;
+  }
   for (int i = 0; i < 10; i ++)
-    ptr[i] = i;
-  for (int i = 0; i < 10; i ++)
-    print arr[i];
+    print *arr[i];
 }
